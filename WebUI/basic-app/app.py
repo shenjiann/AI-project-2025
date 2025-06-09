b22b0368319eb2b00fa310616406f140389423fa
ShenJian <1178563743@qq.com>
2025-06-09 21:27:56 +0800

    Update app.py

--- WebUI/basic-app/app.py
+++ WebUI/basic-app/app.py
@@ -5,54 +5,49 @@ import numpy as np
 import tempfile
 
 
-ui.page_opts(fillable=True)
+# ui.page_opts(fillable=True)
 
-with ui.card():
-    ui.card_header('Input Images')
+with ui.layout_sidebar():
+    with ui.sidebar(bg='#f8f8f8'):
+        ui.input_file('f', 'select a file')
 
-    with ui.layout_sidebar():
-        with ui.sidebar(bg='#f8f8f8'):
-            ui.input_file('f', 'select a file')
+    @reactive.calc
+    def parsed_file():
+        f_path = input.f()[0]['datapath']
+        return f_path
 
-        @reactive.calc
-        def parsed_file():
-            f_path = input.f()[0]['datapath']
-            return f_path
-
-        @reactive.calc
-        def img_array():
-            img = Image.open(parsed_file()).convert('L')
-            return np.array(img)
+    @reactive.calc
+    def img_array():
+        img = Image.open(parsed_file()).convert('L')
+        return np.array(img)
 
 
-        @render.image
-        def image():
-            img = Image.fromarray(img_array())
-            tmp = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
-            img.save(tmp.name)
-            img = {'src': tmp.name, 'width': '100px'}
-            return img
+    @render.image
+    def image():
+        img = Image.fromarray(img_array())
+        tmp = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
+        img.save(tmp.name)
+        img = {'src': tmp.name, 'width': '100px'}
+        return img
 
 
 
-with ui.card():
-    ui.card_header('Conv Kernels')
 
-    with ui.layout_sidebar():
-        with ui.sidebar(bg='#f8f8f8'):
-            ui.input_select(
-                'conv_kernel_select',
-                'select a convolution kernel',
-                {
-                    'Gaussian': 'Gaussian',
-                    'Sharpen': 'Sharpen',
-                    'vertical edge': 'vertical edge',
-                    'horizontal edge': 'horizontal edge',
-                }
-            )
-            ui.input_slider('size', 'size', 1, 10, 1)
-            ui.input_slider('padding', 'padding', 0, 10, 0)
-            ui.input_slider('stride', 'stride', 1, 10, 1)
+with ui.layout_sidebar():
+    with ui.sidebar(bg='#f8f8f8'):
+        ui.input_select(
+            'conv_kernel_select',
+            'select a convolution kernel',
+            {
+                'Gaussian': 'Gaussian',
+                'Sharpen': 'Sharpen',
+                'vertical edge': 'vertical edge',
+                'horizontal edge': 'horizontal edge',
+            }
+        )
+        ui.input_slider('size', 'size', 1, 10, 1)
+        ui.input_slider('padding', 'padding', 0, 10, 0)
+        ui.input_slider('stride', 'stride', 1, 10, 1)
 
 
 
