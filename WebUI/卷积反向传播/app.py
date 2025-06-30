6b0c98059ec8247823b7d3eddc6e535dbc3fdef4
ShenJian <1178563743@qq.com>
2025-06-30 17:27:57 +0800

    minor modify

--- WebUI/卷积反向传播/app.py
+++ WebUI/卷积反向传播/app.py
@@ -2,25 +2,16 @@ from shiny import App, ui, render, reactive, Session
 import numpy as np
 from pathlib import Path
 from utility import *
-import torch
-import torch.nn.functional as F
 
-Z = torch.randint(-5, 6, (1, 1, 3, 3), dtype=torch.float32, requires_grad=True)
-W = torch.randint(-3, 4, (1, 1, 2, 2), dtype=torch.float32, requires_grad=True)
-Z0 = F.conv2d(Z, W)
-dZ0 = torch.randint(-3, 3, Z0.shape, dtype=torch.float32)
-Z0.backward(dZ0)
-dZ = Z.grad
-dW = W.grad
 
 app_ui = ui.page_fluid(
     ui.include_css(Path(__file__).parent/"www/styles.css"),
     ui.HTML((Path(__file__).parent / "www/mathjax_config.html").read_text(encoding="utf-8")),
     # ui.output_image("threedep"),
     ui.panel_title("卷积反向传播"),
-    ui.h4('输入矩阵'),
+    ui.h4('Z'),
     ui.output_ui("Z_html"),
-    ui.h4('卷积核'),
+    ui.h4('W'),
     ui.output_ui("W_html"),
     ui.h4('dZ0'),
     ui.output_ui("dZ0_html"),
@@ -46,8 +37,7 @@ def server(input, output, session):
     @output
     @render.ui
     def Z_html():
-        matrix_html = matrix_to_html(Z, highlight=[(0, 0), (1, 1), (2, 2)])
-        return ui.HTML(matrix_to_html(Z))
+        return ui.HTML(matrix_to_html(Z, highlight=[(0,0), (2,2), (0, 2)]))
     
     @output
     @render.ui
--- WebUI/卷积反向传播/utility.py
+++ WebUI/卷积反向传播/utility.py
@@ -1,5 +1,19 @@
 import numpy as np
 import torch
+import torch.nn.functional as F
+
+torch.manual_seed(42)
+Z = torch.randint(
+    low=-5, high=6, size=(1, 1, 3, 3), 
+    dtype=torch.float32, requires_grad=True)
+W = torch.randint(
+    low=-3, high=4, size=(1, 1, 2, 2), 
+    dtype=torch.float32, requires_grad=True)
+Z0 = F.conv2d(Z, W)
+dZ0 = torch.randint(-3, 3, Z0.shape, dtype=torch.float32)
+Z0.backward(dZ0)
+dZ = Z.grad
+dW = W.grad
 
 def matrix_to_html(matrix: np.ndarray | torch.Tensor, highlight: list[tuple[int, int]] = None) -> str:
     """
